dae8e8fcdc8234726d1668b1db2d618bd9d15c94
Josélio Samy <joseliobackup@gmail.com>
2021-01-25 14:44:36 -0300

    footer and logo added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
@@ -32,6 +33,7 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}> 
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>The voley spike</h1>
@@ -52,6 +54,7 @@ export default function Home() {
               <p>lorem impson erod smasr</p>
             </Widget.Content>
         </Widget>
+        <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/joseliosamy"/>
     </QuizBackground>
